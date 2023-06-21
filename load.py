2788507f8e588dd2261cdd67f247a72c38866568
Zotman03 <zhenglin.ning@outlook.com>
2023-06-20 22:29:15 -0700

    loaded dataset

--- load.py
+++ load.py
@@ -1,1 +1,12 @@
-print("hello world")
+from datasets import load_dataset
+
+# Load IMDb dataset and download specific rows
+dataset = load_dataset('coastalcph/fairlex', 'scotus', split='test')
+itera = 1
+for example in dataset:
+    if(itera == 2):
+        break
+    text = example['text']
+    label = example['label']
+    itera += 1
+    print(text, label)
