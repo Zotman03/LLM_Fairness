9a0bc5ed5d387a438fa954afc11f4b17534eda98
Zotman03 <zhenglin.ning@outlook.com>
2023-06-26 14:42:00 -0700

    Can connect to API

--- load.py
+++ load.py
@@ -2,34 +2,24 @@ from datasets import load_dataset
 import openai
 #build GPT response API
 
-# Load IMDb dataset and download specific rows
-dataset = load_dataset('coastalcph/fairlex', 'scotus', split='test')
-itera = 1
-for example in dataset:
-    if(itera == 2):
-        break
-    text = example['text']
-    label = example['label']
-    itera += 1
-    print(text, label)
+# dataset = load_dataset('coastalcph/fairlex', 'scotus', split='test')
+# itera = 1
+# for example in dataset:
+#     if(itera == 2):
+#         break
+#     text = example['text']
+#     label = example['label']
+#     itera += 1
+#     print(text, label)
 
-#make env file
-openai.api_key = 'YOUR_API_KEY'
+# #make env file
+# openai.api_key = 'YOUR_API_KEY'
 
-def checkGPTresponse(question):
-    prompt = "Question: " + question + "\nAnswer:"
-    response = openai.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=100,
-        n=1,
-        stop=None,
-        temperature=0.7
-    )
-
-    answer = response.choices[0].text.strip().split(":")[1].strip()
-    return answer
-
-question = "What is the capital of France?"
-answer = checkGPTresponse(question)
-print("Answer:", answer)
+completion = openai.ChatCompletion.create(
+  model="gpt-3.5-turbo", 
+  messages = [{"role": "system", "content" : "read the script and answer in three bullet points"},
+{"role": "user", "content" : "what kind of crime is robbery"},
+{"role": "assistant", "content" : "it is a felony"},
+{"role": "user", "content" : "what kind of crime is burning a building?"}]
+)
+print(completion)
