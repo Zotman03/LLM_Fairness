dd25a10b5c0ede28367b750c0a047d889296ab4d
Zotman03 <zhenglin.ning@outlook.com>
2023-07-03 16:57:12 -0700

    First data processing

--- load.py
+++ load.py
@@ -13,44 +13,48 @@ dataset = load_dataset('coastalcph/fairlex', 'scotus', split='test')
 
 # Example data
 text = dataset[0]['text']
-input_ans = dataset[0]['label']
 
 # Numbers
 total = 0
 total_right = 0
 buffer = 0
-
+is_first = True
 # Loop now
 for example in dataset:
-    if(total == 100):
-      break
-    
-    input_text = example['text']
-    input_ans = example['label']
-    completion = openai.ChatCompletion.create(
-      temperature=0,
-      model="gpt-3.5-turbo", 
-      messages = [{"role": "system", "content" : "read the script and predict the relevant issue area in these categories (0, Criminal Procedure), (1, Civil Rights), (2, First Amendment), (3, Due Process), (4, Privacy), (5, Attorneys), (6, Unions), (7, Economic Activity), (8, Judicial Power), (9, Federalism), (10, Interstate Relations), (11, Federal Taxation), (12, Miscellaneous), (13, Private Action)."},
-      {"role": "user", "content" : "what would the predicted label for this" + text[:4000] + "will be? Respond with just the label number"},
-      {"role": "assistant", "content" : "2"},
-      {"role": "user", "content" : "what would the predicted label for this" + input_text[:4000] + "will be? Respond with just the label number"}]
-    )
-
-    if(completion['choices'][0]['message']['content'] == str(input_ans)): # Check if the predicted label is equal to actual label.
-        total_right += 1
+    if(is_first is True):
+       is_first = False
+       continue
     else:
-       if(len(completion['choices'][0]['message']['content']) > 1):
-           match = re.search(r'\d+', completion['choices'][0]['message']['content'])
-           if match:
-              extracted_number = str(match.group())
-              if extracted_number == str(input_ans):
-                 total_right += 1
-    total += 1
-    print(total, " out of 100 complete")
-    buffer += 1
-    if(buffer == 10):
-      time.sleep(10)
-      buffer = 0
-    
+      if(total == 100):
+        break
+      
+      input_text = example['text']
+      input_ans = example['label']
+      completion = openai.ChatCompletion.create(
+        temperature=0,
+        model="gpt-3.5-turbo", 
+        messages = [{"role": "system", "content" : "read the script and predict the relevant issue area in these categories (0, Criminal Procedure), (1, Civil Rights), (2, First Amendment), (3, Due Process), (4, Privacy), (5, Attorneys), (6, Unions), (7, Economic Activity), (8, Judicial Power), (9, Federalism), (10, Interstate Relations), (11, Federal Taxation), (12, Miscellaneous), (13, Private Action)."},
+        {"role": "user", "content" : "what would the predicted label for this" + text[:4000] + "will be? Respond with just the label number"},
+        {"role": "assistant", "content" : "2"},
+        {"role": "user", "content" : "what would the predicted label for this" + input_text[:4000] + "will be? Respond with just the label number"}]
+      )
 
+      if(completion['choices'][0]['message']['content'] == str(input_ans)): # Check if the predicted label is equal to actual label.
+          total_right += 1
+      else:
+        if(len(completion['choices'][0]['message']['content']) > 1):
+            match = re.search(r'\d+', completion['choices'][0]['message']['content'])
+            if match:
+                extracted_number = str(match.group())
+                if extracted_number == str(input_ans):
+                  total_right += 1
+      total += 1
+      print(total, " out of 100 complete")
+      buffer += 1
+      if(buffer == 10):
+        time.sleep(10)
+        buffer = 0   
+
+print(total_right)
+print(total)
 print(total_right / total)
