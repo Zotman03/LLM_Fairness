606409c9be7cdd7d7e943d7e22bd1351ca80fea2
Zotman03 <zhenglin.ning@outlook.com>
2023-07-24 23:26:02 -0700

    increased dataset + minor updates

--- GPT3.5SCOTUS.py
+++ GPT3.5SCOTUS.py
@@ -12,7 +12,7 @@ import math
 load_dotenv()
 api_key = os.getenv('NEXT_PUBLIC_OPENAI_API_KEY')
 openai.api_key = api_key
-dataset = load_dataset('coastalcph/fairlex', 'scotus', split='test')
+dataset = load_dataset('coastalcph/fairlex', 'scotus', split='train')
 
 # Example data
 text = dataset[0]['text']
@@ -36,7 +36,7 @@ for example in dataset:
        is_first = False
        continue
     else:
-      if(total == 100):
+      if(total == 1000):
         break
       
       input_text = example['text']
@@ -47,7 +47,7 @@ for example in dataset:
         model="gpt-3.5-turbo", 
         messages = [{"role": "system", "content" : "read the script and predict the relevant issue area in these categories (0, Criminal Procedure), (1, Civil Rights), (2, First Amendment), (3, Due Process), (4, Privacy), (5, Attorneys), (6, Unions), (7, Economic Activity), (8, Judicial Power), (9, Federalism), (10, Interstate Relations), (11, Federal Taxation), (12, Miscellaneous), (13, Private Action)."},
         {"role": "user", "content" : "what would the predicted label for this" + text[:4000] + "will be? Respond with just the label number"},
-        {"role": "assistant", "content" : "2"},
+        {"role": "assistant", "content" : "9"},
         {"role": "user", "content" : "what would the predicted label for this" + input_text[:4000] + "will be? Respond with just the label number"}]
       )
 
